10c9769db2c9d609404f1afe040c602c7955353e
Dnlln <ilin.daniil@gmail.com>
2026-05-18 15:11:41 +0300

    refactor: add src/preprocessing/__init__.py

---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,6 @@
+from .datasets import load_unsw_nb15, load_adult, load_sms_spam, load_mnist_binary, load_all_datasets, stratified_subsample
+
+__all__ = [
+    "load_unsw_nb15", "load_adult", "load_sms_spam",
+    "load_mnist_binary", "load_all_datasets", "stratified_subsample",
+]
